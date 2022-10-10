035ba92fe200aad55f2817cb62a606f047444841
Hayden Quinn <hayden@increnaut.io>
2022-10-10 15:56:03 +1100

    (invalid-message-channel) Completed 'declare an invalid message queue, durable' in DataTypeChannelConsumer

--- invalid-message-channel/SimpleMessaging/DataTypeChannelConsumer.cs
+++ invalid-message-channel/SimpleMessaging/DataTypeChannelConsumer.cs
@@ -68,9 +68,10 @@ namespace SimpleMessaging
             //messages that we nack without requeue will go here
             // Declare an invalid message queue exchange, direct and durable
             _channel.ExchangeDeclare(InvalidMessageExchangeName, ExchangeType.Direct, true);
-            // TODO: declare an invalid message queue, durable
+            // declare an invalid message queue, durable
+            _channel.QueueDeclare(invalidMessageQueueName, true, false, false);
             // TODO: bind the queue to the exchange
- 
+
         }
 
         /// <summary>
